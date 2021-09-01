21df6a1881010c5ceb5018d2a57b2a056424e41b
Rathish Kumar <rathishudhay@gmail.com>
2021-09-01 16:04:32 +0530

    frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,18 @@
+import { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import io from "socket.io-client";
 
-const socket = io.connect('localhost:5000');
-socket.emit('test');
+
 function App() {
+  const [socketReceivedString, setSocketReceivedString] = useState(null);
+  const socket = io.connect('localhost:5000');
+  socket.emit('test');
+  socket.on('test-received', (stringToSet) => {
+    console.log("received");
+    setSocketReceivedString(stringToSet);
+  })
+
   return (
     <div className="App">
       <header className="App-header">
@@ -18,6 +26,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
+          {socketReceivedString != null && socketReceivedString}
           Learn React
         </a>
       </header>
